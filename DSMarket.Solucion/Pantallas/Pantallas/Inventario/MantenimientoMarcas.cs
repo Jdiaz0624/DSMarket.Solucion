fed88f944fff379c2e8ae53eabea82f7a4152c6f
Juan Marcelino Medina Diaz <ing.juanmarcelinom.diaz@gmail.com>
2020-06-05 21:21:57 -0400

    Marcas

---
+++ DSMarket.Solucion/Pantallas/Pantallas/Inventario/MantenimientoMarcas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSMarket.Solucion.Pantallas.Pantallas.Inventario
+{
+    public partial class MantenimientoMarcas : Form
+    {
+        public MantenimientoMarcas()
+        {
+            InitializeComponent();
+        }
+        public DSMarket.Logica.Comunes.VariablesGlobales VariablesGlobales = new Logica.Comunes.VariablesGlobales();
+        private void MantenimientoMarcas_Load(object sender, EventArgs e)
+        {
+            this.BackColor = SystemColors.Control;
+            txtClaveSeguridad.BackColor = Color.WhiteSmoke;
+            txtMArca.BackColor = Color.WhiteSmoke;
+
+            if (VariablesGlobales.Accion == "INSERT")
+            {
+                lbTitulo.Text = "CREAR NUEVO REGISTRO";
+                lbTitulo.ForeColor = Color.WhiteSmoke;
+                btnGuardar.Text = "Guardar registro";
+                lbclaveSeguridad.Visible = false;
+                txtClaveSeguridad.Visible = false;
+            }
+            else if (VariablesGlobales.Accion == "UPDATE")
+            {
+                lbTitulo.Text = "MODIFICAR REGISTRO SELECCIONADO";
+                lbTitulo.ForeColor = Color.WhiteSmoke;
+                btnGuardar.Text = "Modificar registro";
+                lbclaveSeguridad.Visible = true;
+                txtClaveSeguridad.Visible = true;
+                txtClaveSeguridad.PasswordChar = '•';
+            }
+        }
+
+        private void PCerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            DSMarket.Solucion.Pantallas.Pantallas.Inventario.MarcasConsulta consulta = new MarcasConsulta();
+            consulta.ShowDialog();
+        }
+
+        private void MantenimientoMarcas_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.UserClosing:
+                    e.Cancel = true;
+                    break;
+            }
+        }
+    }
+}
--- DSMarket.Solucion/Pantallas/Pantallas/Inventario/MarcasConsulta.cs
+++ DSMarket.Solucion/Pantallas/Pantallas/Inventario/MarcasConsulta.cs
@@ -16,5 +16,49 @@ namespace DSMarket.Solucion.Pantallas.Pantallas.Inventario
         {
             InitializeComponent();
         }
+
+        private void PCerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void MarcasConsulta_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.UserClosing:
+                    e.Cancel = true;
+                    break;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DSMarket.Solucion.Pantallas.Pantallas.Inventario.MantenimientoMarcas Mantenimiento = new MantenimientoMarcas();
+            Mantenimiento.VariablesGlobales.Accion = "INSERT";
+            Mantenimiento.ShowDialog();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DSMarket.Solucion.Pantallas.Pantallas.Inventario.MantenimientoMarcas Mantenimiento = new MantenimientoMarcas();
+            Mantenimiento.VariablesGlobales.Accion = "UPDATE";
+            Mantenimiento.ShowDialog();
+        }
+
+        private void MarcasConsulta_Load(object sender, EventArgs e)
+        {
+            lbTitulo.Text = "CONSULTA DE MARCAS";
+            lbTitulo.ForeColor = Color.WhiteSmoke;
+            lbCantidadRegistrosTitulo.ForeColor = Color.WhiteSmoke;
+            lbCantidadRegistrosVariable.ForeColor = Color.WhiteSmoke;
+            this.BackColor = SystemColors.Control;
+            txtMarca.BackColor = Color.WhiteSmoke;
+            txtNumeroPagina.BackColor = Color.WhiteSmoke;
+            txtNumeroRegistros.BackColor = Color.WhiteSmoke;
+            dtListado.BackgroundColor = SystemColors.Control;
+        }
     }
 }
