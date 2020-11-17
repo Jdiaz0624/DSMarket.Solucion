68cfa9f2f290d05726e2fd4b12e3fcf9b5e012bb
Ing. Juan Marcelino <Ing. Juan Marcelino@JDIAZ0624>
2020-11-17 07:45:01 -0400

    ttt

---
+++ DSMarket.Solucion/Pantallas/Pantallas/Graficos/CantidadMArcas.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DSMarket.Solucion.Pantallas.Pantallas.Graficos
+{
+    partial class CantidadMArcas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CantidadMArcas));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GraficoMarcas = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GraficoMarcas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 30;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.Icon = ((System.Drawing.Icon)(resources.GetObject("errorProvider1.Icon")));
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.GraficoMarcas);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(774, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // GraficoMarcas
+            // 
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.GraficoMarcas.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.GraficoMarcas.Legends.Add(legend1);
+            this.GraficoMarcas.Location = new System.Drawing.Point(6, 19);
+            this.GraficoMarcas.Name = "GraficoMarcas";
+            this.GraficoMarcas.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 4;
+            this.GraficoMarcas.Series.Add(series1);
+            this.GraficoMarcas.Size = new System.Drawing.Size(759, 401);
+            this.GraficoMarcas.TabIndex = 0;
+            this.GraficoMarcas.Text = "chart1";
+            // 
+            // CantidadMArcas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 450);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CantidadMArcas";
+            this.Text = "CantidadMArcas";
+            this.Load += new System.EventHandler(this.CantidadMArcas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GraficoMarcas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart GraficoMarcas;
+    }
+}
---
+++ DSMarket.Solucion/Pantallas/Pantallas/Graficos/CantidadMArcas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DSMarket.Solucion.Pantallas.Pantallas.Graficos
+{
+    public partial class CantidadMArcas : Form
+    {
+        public CantidadMArcas()
+        {
+            InitializeComponent();
+        }
+        int[] Cantidad = new int[10];
+        string[] Marca = new string[10];
+
+        private void ObtenerInformacion() {
+
+            int Contador = 0;
+            SqlConnection Conexion = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DSMarketConexion"].ConnectionString);
+
+            SqlCommand comando = new SqlCommand("SELECT COUNT(*) AS Cantidad ,m.Descripcion as Marca from Inventario.Producto pro inner join Inventario.Marcas m on m.IdMarca = pro.IdMarca group by m.Descripcion", Conexion);
+            Conexion.Open();
+            SqlDataReader reader = comando.ExecuteReader();
+            while (reader.Read()) {
+                Cantidad[Contador] = Convert.ToInt32(reader.GetInt32(0));
+                Marca[Contador] = reader.GetString(1);
+                Contador++;
+            }
+            GraficoMarcas.Series["Series1"].Points.DataBindXY(Marca,Cantidad );
+        }
+
+        private void CantidadMArcas_Load(object sender, EventArgs e)
+        {
+            ObtenerInformacion();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
